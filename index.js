a708401a2f42b4798789151ace3239606668b974
cardo88 <cardo88@gmail.com>
2023-11-06 19:02:54 -0300

    RC - previo a merge form main

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
+const io = require('./config/websocket');
 
 // Creamos el servidor
 const app = express();
 
+const server = require('http').createServer(app);
+const socketIo = io(server);
+
 // Conectamos a la BD
 conectarDB();
-app.use(cors())
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/questions', require('./routes/trivia'));
 app.use('/api/hangman', require('./routes/hangman'));
 app.use('/api/rooms', require('./routes/room'));
 
-app.listen(4000, () => {
-    console.log('El servidor esta corriendo perfectamente')
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Info msg: index.js >> El servidor está corriendo correctamente en el puerto ${PORT}`)
 })

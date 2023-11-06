12386ddddcf85c3a0d4788f90b6cbe6bb54b7414
Amanda Seara <amandadenissese@gmail.com>
2023-11-06 12:55:31 -0300

    proposals

---
+++ controllers/proposalsController.js
@@ -0,0 +1,51 @@
+const Proposal = require('../models/proposal');
+
+exports.createProposal = async (req, res) => {
+    try {
+        const existingProposal = await Room.findOne({ name: req.body.name });
+
+        if (existingProposal) {
+            return res.status(400).json({ msg: 'Ya existe una propuesta con este nombre' });
+        }
+        const { title, activities } = req.body;
+        const newProposal = new Proposal({ title, activities });
+        await newProposal.save();
+        res.status(201).json(newProposal);
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            // Manejar errores de validación
+            const validationErrors = {};
+            for (const field in error.errors) {
+                validationErrors[field] = error.errors[field].message;
+            }
+            res.status(400).json({ errors: validationErrors });
+        } else {
+            console.log(error);
+            res.status(500).send('Hubo un error');
+        }
+    }
+};
+
+exports.getProposals = async (req, res) => {
+    try {
+        const proposals = await Proposal.find().populate('activities');
+        res.status(200).json(proposals);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+};
+
+exports.deleteProposal = async (req, res) => {
+    try {
+        let proposal = await req.findById(req.params.id);
+        if (!proposal) {
+            res.status(404).json({ msg: 'No existe la propuesta' })
+        }
+        await req.findOneAndRemove({ _id: req.params.id })
+        res.json({ msg: 'Propuesta eliminada con éxito' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+};

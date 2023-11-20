b60f253cbb4f86a957b43c2371005df06eb0fb2a
lukibenitez <lucasbenitezl@outlook.com>
2023-11-20 18:35:01 -0300

    Lucas: update

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORTwebsocket = process.env.PORT || 5000;
+const PORTwebsocket = process.env.PORT || 6000;
 
 server.listen(PORTwebsocket, function () {
     console.log(`Info msg: index.js >> WebSocket listo y escuchando por el puerto: ` + PORTwebsocket)
